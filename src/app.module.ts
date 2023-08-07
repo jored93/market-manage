47b66aa4b64718cefdcbeeba9c3d7545a553af72
Jorman Orrala Lainez <eduardorrala@live.com>
2023-08-07 13:55:48 -0500

    feat[config]: Update to add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import { ConfigModule } from '@nestjs/config';
+
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+
 @Module({
-  imports: [ProductsModule, UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    HttpModule,
+    ProductsModule,
+    UsersModule,
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const request = http.get('https://jsonplaceholder.typicode.com/todos');
+        const tasks = await lastValueFrom(request);
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
