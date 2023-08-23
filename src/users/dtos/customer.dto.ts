5d61748bfcb635f820fa77ad28fc14084d7233e0
Jorman Orrala Lainez <eduardorrala@live.com>
2023-08-22 22:20:49 -0500

    feat[relations]: Update to add relation 1:1 user->customer

--- src/users/dtos/customer.dto.ts
+++ src/users/dtos/customer.dto.ts
@@ -1,18 +1,15 @@
 import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 
 export class CreateCustomerDto {
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly name: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly lastName: string;
 
-  @ApiProperty()
   @IsPhoneNumber()
   @IsNotEmpty()
   readonly phone: string;
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CustomerController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
+import { Customer } from './entities/customer.entity';
+
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
+import { User } from './entities/user.entity';
+
 import { ProductsModule } from 'src/products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, TypeOrmModule.forFeature([Customer, User])],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
